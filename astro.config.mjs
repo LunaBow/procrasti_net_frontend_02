7e21d3490f3b1caf621a25bf91b35e6b52be43d5
LunaBow <161691980+LunaBow@users.noreply.github.com>
2026-03-01 18:27:18 +0100

    astro config fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,11 @@
-// @ts-nocheck
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import svelte from '@astrojs/svelte';
-
 import tailwindcss from '@tailwindcss/vite';
 
-import netlify from '@astrojs/netlify';
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
-
-  adapter: netlify(),
-
+  adapter: node({ mode: 'standalone' }),
   integrations: [svelte()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  vite: { plugins: [tailwindcss()] }
 });
