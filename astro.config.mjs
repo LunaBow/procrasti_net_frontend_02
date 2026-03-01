79ba29365bd739cf34035bd5a6081e927e0e7d3b
LunaBow <161691980+LunaBow@users.noreply.github.com>
2026-03-01 19:47:00 +0100

    astro lines in package making vite mad

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
----
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
----
+
 export default defineConfig({
   output: 'server',
   adapter: node({ mode: 'standalone' }),
