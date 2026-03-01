8d0e87e44692fb055913560a0247a2f870211c00
LunaBow <161691980+LunaBow@users.noreply.github.com>
2026-03-01 18:56:21 +0100

    login edit: remove unused

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
+---
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
-
+---
 export default defineConfig({
   output: 'server',
   adapter: node({ mode: 'standalone' }),
   integrations: [svelte()],
-  vite: { plugins: [tailwindcss()] }
+  vite: { plugins: [tailwindcss()] },
+  server: { host: true }
 });
